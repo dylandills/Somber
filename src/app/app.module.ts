e5cd3031362aa057a447aa105b69fced1c6a5d37
Dylan Dills <dylan.dills@gmail.com>
2017-08-31 16:05:33 -0700

    successful routing with navigation on all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -28,6 +29,7 @@ import { ContactComponent } from './contact/contact.component';
     HttpModule,
     routing
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
